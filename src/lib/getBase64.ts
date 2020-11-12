022c923b6cba3aca5256180e8b525328b8538424
Mark Njoroge <marknjo7@gmail.com>
2020-11-12 14:21:09 +0300

    implement adding blurred images to photo.blurredDataUrl - for placeholders

--- src/lib/getBase64.ts
+++ src/lib/getBase64.ts
@@ -1,4 +1,5 @@
 import { getPlaiceholder } from 'plaiceholder';
+import { ImagesResults, Photo } from '../model/Images';
 
 async function getBase64(imageUrl: string) {
   try {
@@ -16,4 +17,23 @@ async function getBase64(imageUrl: string) {
   } catch (error) {
     if (error instanceof Error) console.log(error.stack);
   }
+}
+
+export async function addBlurredDataUrls(images: ImagesResults) {
+  // make all requests at once instead of awaiting each one - avoids a waterfall
+  const base64Promises = images.photos.map((photo) =>
+    getBase64(photo.src.large)
+  );
+
+  // resolve all requests in order
+  const base64Results = await Promise.all(base64Promises);
+
+  // add blurredDataUrl to the image
+  const photosWithBlur: Photo[] = images.photos.map((photo, i) => {
+    photo.blurredDataUrl = base64Results[i];
+
+    return photo;
+  });
+
+  return photosWithBlur;
 }
