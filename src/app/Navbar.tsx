a384a1b65ca0b9767a6424df4fd38b4ae9c970b9
Mark Njoroge <marknjo7@gmail.com>
2020-11-14 15:14:45 +0300

    add NavBar component styles

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <header className='bg-black'>
-      <nav>
-        <h1>
+    <header className='bg-black sticky top-0 z-10'>
+      <nav className='max-w-6xl mx-auto flex sm:justify-between mb:flex-column gap-4 items-center p-4 font-bold text-white'>
+        <h1 className='text-3xl md:text-2xl text-center whitespace-normal'>
           <Link href='/'>Next Gallery</Link>
         </h1>
       </nav>
