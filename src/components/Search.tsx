cb2470c8f7a815f8e0a31972e7462500c8394d9a
Mark Njoroge <marknjo7@gmail.com>
2020-11-16 16:37:08 +0300

    fix Search component not handling search appropriently

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,29 +1,38 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 
 export default function Search() {
   const [search, setSearch] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const router = useRouter();
 
   async function searchHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
+    setIsSearching(true);
+
     router.push(`/results/${search}`);
+    setIsSearching(false);
     setSearch('');
+    inputRef.current!.focus();
   }
 
   return (
     <form
-      onClick={searchHandler}
+      onSubmit={searchHandler}
       className='flex justify-center md:justify-between'>
       <input
         type='text'
         name='search'
-        value={search}
+        placeholder='Search'
+        ref={inputRef}
+        value={isSearching ? `Searching ${search}` : search}
         onChange={(e) => setSearch(e.target.value)}
-        className='bg-white p-2 w-[260px] sm:w-80 text-xl rounded-xl text-black'
+        className='bg-white p-2 px-3 w-[260px] sm:w-80 text-xl rounded-xl text-black'
       />
     </form>
   );
