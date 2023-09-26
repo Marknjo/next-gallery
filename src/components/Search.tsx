a5d0472210aa3e53b98071c79a1ec015f0d3d99c
Mark Njoroge <marknjo7@gmail.com>
2023-09-26 16:10:59 +0300

    refactor Search component to reset & focus form field & remove unused useState

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -5,7 +5,6 @@ import { FormEvent, useRef, useState } from 'react';
 
 export default function Search() {
   const [search, setSearch] = useState('');
-  const [isSearching, setIsSearching] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const router = useRouter();
@@ -13,12 +12,11 @@ export default function Search() {
   async function searchHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    setIsSearching(true);
-
-    if (search) router.push(`/results/${search}`);
-    setIsSearching(false);
-    setSearch('');
-    inputRef.current!.focus();
+    if (search) {
+      setSearch('');
+      inputRef.current!.focus();
+      router.push(`/results/${search.trim()}`);
+    }
   }
 
   return (
@@ -30,8 +28,8 @@ export default function Search() {
         name='search'
         placeholder='Search'
         ref={inputRef}
-        value={isSearching ? `Searching ${search}` : search}
-        onChange={(e) => setSearch(e.target.value.trim())}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         className='bg-white p-2 px-3 w-[260px] sm:w-80 text-xl rounded-xl text-black'
       />
     </form>
