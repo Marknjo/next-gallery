fe89dd725f772c2d62320a2b039200a5bb18ada4
Mark Njoroge <marknjo7@gmail.com>
2023-09-27 09:14:25 +0300

    implement fetchImage method in the server lib

---
+++ src/lib/fetchImage.ts
@@ -0,0 +1,35 @@
+import { getPlaiceholder } from 'plaiceholder';
+import { Photo, SingleImageSchema } from '../model/Images';
+import env from './env';
+import { getBase64 } from './getBase64';
+
+export default async function fetchImage(
+  id: string
+): Promise<Photo | undefined> {
+  try {
+    const url = `https://api.pexels.com/v1/photos/${id}`;
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: env.PEXELS_API_KEY,
+      },
+    });
+
+    if (!res.ok) throw new Error('Fetch Images error!\n');
+
+    const photoResult: Photo = await res.json();
+
+    // Parse data with zod
+    const parsedData = SingleImageSchema.parse(photoResult);
+
+    if (!parsedData) return undefined;
+
+    const base64Img = await getBase64(parsedData.src.large);
+
+    parsedData.blurredDataUrl = base64Img;
+
+    return parsedData;
+  } catch (error) {
+    if (error instanceof Error) console.log(error.stack);
+  }
+}
