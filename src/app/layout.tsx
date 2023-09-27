98264140a9eb6dda915fdb3325ca6615b20b2143
Mark Njoroge <marknjo7@gmail.com>
2023-09-27 09:15:28 +0300

    prep layout page for parallel route with @modal component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import Navbar from '../components/Navbar';
+import React from 'react';
 
 export const revalidate = 3600; // update to 3600
 
@@ -14,14 +15,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <html lang='en'>
       <body className={roboto.className}>
         <Navbar />
         <main className='max-w-6xl mx-auto'>{children}</main>
+        {modal}
       </body>
     </html>
   );
