7c0bbde78e79794093bc55b72c35f5fce35a57ec
Mark Njoroge <marknjo7@gmail.com>
2023-09-27 09:08:46 +0300

    refactor ImgContainer to accomodate new feature - parallel routes & modal

--- src/components/ImgContainer.tsx
+++ src/components/ImgContainer.tsx
@@ -5,36 +5,44 @@ import Link from 'next/link';
 
 interface IProps {
   photo: Photo;
+  className?: string;
+  useTinyWidth?: boolean;
 }
 
-export default function ImgContainer({ photo }: IProps) {
+export default function ImgContainer({
+  photo,
+  className,
+  useTinyWidth = false,
+}: IProps) {
+  const imgWidth = useTinyWidth
+    ? Number(photo.src.large.split('&w=').at(-1)) *
+      (photo.height > photo.width ? 0.5 : 1)
+    : 250;
   const widthHeightRatio = photo.height / photo.width;
-  const galleryHeight = Math.ceil(250 * widthHeightRatio);
+  const galleryHeight = Math.ceil(imgWidth * widthHeightRatio);
   const photoSpans = Math.ceil(galleryHeight / 10) + 1;
 
   return (
     <article
-      className='w-[250px] justify-self-center'
+      className={`justify-self-center ${className ? className : 'w-[250px]'}`}
       style={{
-        gridRow: `span ${photoSpans}`,
+        ...(useTinyWidth
+          ? { width: `${imgWidth}px`, height: 'auto' }
+          : { gridRow: `span ${photoSpans}` }),
       }}>
-      <Link
-        href={photo.url}
-        target='_blank'
-        referrerPolicy='origin'
-        className='grid place-content-center'>
+      <Link href={`/photos/${photo.id}`} className='grid place-content-center'>
         <div className='rounded-xl overflow-hidden group'>
           <Image
             src={photo.src.large}
             height={galleryHeight}
-            width={250}
+            width={imgWidth}
             alt={photo.alt}
-            sizes='250px'
-            className='hover:opacity-75 cursor-pointer'
+            sizes={`${imgWidth}px`}
+            className='hover:opacity-75 cursor-pointer  '
             placeholder='blur'
             blurDataURL={photo.blurredDataUrl}
             priority={false}
-            style={{ width: 250, height: galleryHeight }}
+            style={{ width: imgWidth, height: galleryHeight }}
           />
         </div>
       </Link>
